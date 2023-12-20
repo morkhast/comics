b4d157c3a3cf98f940ee4000e476eb881f6b8059
pocketcat0 <pockettcat0@gmail.com>
2023-12-21 01:21:52 +0400

    init

--- scripts.js
+++ scripts.js
@@ -1,9 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
     const comicContainer = document.getElementById('comic-container');
+    const comicPages = document.querySelectorAll('.comic-page');
 
+    let currentPageIndex = 0;
+
+    function scrollToNextPage() {
+        if (currentPageIndex < comicPages.length - 1) {
+            currentPageIndex++;
+            comicPages[currentPageIndex].scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    function scrollToPrevPage() {
+        if (currentPageIndex > 0) {
+            currentPageIndex--;
+            comicPages[currentPageIndex].scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    // Добавляем обработчик события прокрутки колеса мыши
     document.addEventListener('wheel', function (event) {
-        // Прокручиваем страницу вверх или вниз при помощи колеса мыши
-        comicContainer.scrollTop += event.deltaY;
+        if (event.deltaY > 0) {
+            scrollToNextPage();
+        } else if (event.deltaY < 0) {
+            scrollToPrevPage();
+        }
+    });
+
+    // Добавляем обработчик события клавиш клавиатуры
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'ArrowDown') {
+            scrollToNextPage();
+        } else if (event.key === 'ArrowUp') {
+            scrollToPrevPage();
+        }
     });
 });
 
